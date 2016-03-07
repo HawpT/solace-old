a0711f3716a8f41b4ee1ed926d4de7f9e32bbfe7
HawpT <kshaupty@gmail.com>
2016-03-06 19:01:23 -0700

    new file:   Assets/_scripts/ControlSwitch.cs 	new file:   Assets/_scripts/ControlSwitch.cs.meta 	new file:   Assets/_scripts/FlyingRigidbodyFirstPersonController.cs 	new file:   Assets/_scripts/FlyingRigidbodyFirstPersonController.cs.meta 	new file:   Assets/_scripts/ForceToAcessibleVolume.cs 	new file:   Assets/_scripts/ForceToAcessibleVolume.cs.meta 	new file:   Assets/_scripts/FreeCamera.cs 	new file:   Assets/_scripts/FreeCamera.cs.meta 	new file:   Assets/_scripts/MouseLock.cs 	new file:   Assets/_scripts/MouseLock.cs.meta 	new file:   Assets/_scripts/PlayerController.cs 	new file:   Assets/_scripts/PlayerController.cs.meta 	new file:   Assets/_scripts/QualitySetup.cs 	new file:   Assets/_scripts/QualitySetup.cs.meta 	new file:   Assets/_scripts/SkyboxReplacer.cs 	new file:   Assets/_scripts/SkyboxReplacer.cs.meta 	new file:   Assets/_scripts/twinkle.cs 	new file:   Assets/_scripts/twinkle.cs.meta 	new file:   Assets/_solace/Action.cs 	new file:   Assets/_solace/Action.cs.meta 	new file:   Assets/_solace/Character.cs 	new file:   Assets/_solace/Character.cs.meta 	new file:   Assets/_solace/Program.cs 	new file:   Assets/_solace/Program.cs.meta

---
+++ Assets/_solace/Character.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace solace
+{
+    public class Character
+    {
+        //Dictionary of Dictionaries, keys are string names of each category of attribute
+        private Dictionary<string, Dictionary<string, int>> coreDict; 
+        //Other class variables
+        private string[] attributes;
+        private int level;
+        private double health, maxHealth;
+        private string name;
+
+        //default constuctor
+        public Character()
+        {
+            coreDict = new Dictionary<string, Dictionary<string, int>>();
+            name = "Not Named";
+            health = 10;
+        }
+
+        public Character(string newName)
+        {
+            name = newName;
+            coreDict = new Dictionary<string, Dictionary<string, int>>();
+            health = 10;
+        }
+
+        //overloaded constuctor for pre-built dictionaries
+        public Character(Dictionary<string, Dictionary<string, int>> importedDict)
+        {
+            coreDict = importedDict;
+            name = "Not Named";
+            health = 10;
+        }
+
+        public Character(Dictionary<string, Dictionary<string, int>> importedDict, string newName)
+        {
+            coreDict = importedDict;
+            name = newName;
+            health = 10;
+        }
+
+        //accessor for all attribute names
+        public string[] getAttributeTypes(){ return attributes; } 
+
+        //accessor that returns the dictionary of key/value pairs in a given attribute category
+        public Dictionary<string, int> getValuesByCategory(string category)
+        {
+            try {
+                var save = coreDict[category];
+                return save;
+            }
+            catch(KeyNotFoundException e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+            
+        }
+
+        //accessor that returns a single attribute value given a category and attribute name, returns -1 if not found
+        public int getValue(string category, string attribute) {
+            try
+            {
+                var save = (coreDict[category])[attribute];
+                return save;
+            }
+            catch (KeyNotFoundException e)
+            {
+                Console.WriteLine(e);
+                return -1;
+            }
+        }
+        
+        //accessor for character name
+        public string getName() { return name; }
+
+        //accessor for current health
+        public double getHealth() { return health; }
+
+        //accessor for max health
+        public double getMaxHealth() { return maxHealth; }
+
+        //mutator for single values given a category and attribute name
+        public void setValue(string category, string attribute, int newValue) { (coreDict[category])[attribute] = newValue; return; }
+
+        //mutator for setting a characters level
+        public void setLevel(int newLevel)
+        {
+            if (newLevel > 0)
+                level = newLevel;
+        }
+
+        //mutator for adding or subtractign health
+        public void modHealth(double changeInHealth) { health += changeInHealth; }
+
+        //mutator for changing a characters max health
+        public void setMaxHealth(double newMaxHealth) { maxHealth = newMaxHealth; }
+
+        //overrided tostring
+        public override string ToString()
+        {
+            return "Character name: " + name + "\nCharacter level: " + level;
+        }
+
+    }
+}
---
+++ Assets/_solace/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace solace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Fill Character with test info. ");
+
+            Dictionary<string, int> stats = new Dictionary<string, int>();
+            Dictionary<string, int> skills = new Dictionary<string, int>();
+            Dictionary<string, Dictionary<string, int>> core = new Dictionary<string, Dictionary<string, int>>();
+
+            //populate our core dict
+            core["stats"] = stats;
+            core["skills"] = skills;
+
+            //populate the stats dict
+            stats["strength"] = 0;
+            stats["agility"] = 1;
+            stats["prana"] = 2;
+            stats["intelligence"] = 3;
+            stats["constitution"] = 4;
+            stats["charisma"] = 5;
+            stats["perception"] = 6;
+
+            //populate the skills dict
+            skills["melee"] = 0;
+            skills["marksmanship"] = 1;
+            skills["sorcery"] = 2;
+            skills["empathy"] = 3;
+            skills["engineering"] = 4;
+            skills["smithing"] = 5;
+            skills["trade"] = 6;
+            skills["fortitude"] = 7;
+            skills["piloting"] = 8;
+            skills["ride"] = 9;
+            skills["stealth"] = 10;
+            skills["survival"] = 11;
+            skills["tactics"] = 12;
+            skills["resilience"] = 13;
+            skills["performance"] = 14;
+            skills["intimidate"] = 15;
+            skills["persuasion"] = 17;
+            skills["secondsight"] = 18;
+            skills["scholarship"] = 19;
+            skills["athletics"] = 20;
+
+            int testmax = 500;
+            int testvalue = 10;
+            double testmod = -46.5;
+            Character aphyxia = new Character(core);
+
+            aphyxia.setValue("stats", "strength", 10);
+            Console.Write("Stats should be 10: " + testvalue + " and is: " + aphyxia.getValue("stats","strength") + "\n");
+
+            aphyxia.setMaxHealth(500);
+            Console.Write("Max health should be: " + testmax + " and is: " + aphyxia.getMaxHealth() + "\n");
+
+            aphyxia.modHealth(-46.5);
+            Console.Write("Current health should be: " + (testmax + testmod) + " and is: " + aphyxia.getHealth() + "\n");
+
+            Console.Read();
+
+        }
+    }
+}
