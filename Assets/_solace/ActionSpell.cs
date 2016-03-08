a93477a6aacba58b6eb7c11ea83b444330045064
HawpT <kshaupty@gmail.com>
2016-03-07 20:01:03 -0700

    deleted:    Assets/_solace/Action.cs 	new file:   Assets/_solace/ActionSpell.cs 	new file:   Assets/_solace/ActionSpell.cs.meta 	new file:   Assets/_solace/GameAction.cs 	renamed:    Assets/_solace/Action.cs.meta -> Assets/_solace/GameAction.cs.meta
    	deleted:    Assets/scripts/ControlSwitch.cs
    	deleted:    Assets/scripts/ControlSwitch.cs.meta
    	deleted:    Assets/scripts/FlyingRigidbodyFirstPersonController.cs
    	deleted:    Assets/scripts/FlyingRigidbodyFirstPersonController.cs.meta
    	deleted:    Assets/scripts/ForceToAcessibleVolume.cs
    	deleted:    Assets/scripts/ForceToAcessibleVolume.cs.meta
    	deleted:    Assets/scripts/FreeCamera.cs
    	deleted:    Assets/scripts/FreeCamera.cs.meta
    	deleted:    Assets/scripts/MouseLock.cs
    	deleted:    Assets/scripts/MouseLock.cs.meta
    	deleted:    Assets/scripts/PlayerController.cs
    	deleted:    Assets/scripts/PlayerController.cs.meta
    	deleted:    Assets/scripts/QualitySetup.cs
    	deleted:    Assets/scripts/QualitySetup.cs.meta
    	deleted:    Assets/scripts/SkyboxReplacer.cs
    	deleted:    Assets/scripts/SkyboxReplacer.cs.meta
    	deleted:    Assets/scripts/twinkle.cs
    	deleted:    Assets/scripts/twinkle.cs.meta
    	deleted:    Assets/solace/Action.cs
    	deleted:    Assets/solace/Character.cs
    	deleted:    Assets/solace/Program.cs

---
+++ Assets/_solace/ActionSpell.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace solace
+{
+    class ActionSpell : GameAction
+    {
+        private int spellRange { get { return spellRange; } set { spellRange = value; } }
+        private int spellArea { get { return spellArea; } set { spellArea = value; } }
+        private int spellValue { get { return spellValue; } set { spellValue = value; } }
+
+        //category and attribute being affected by the spell
+        private string spellTargetAttribute { get { return spellTargetAttribute; } set { spellTargetAttribute = value; } }
+        private string spellTargetCategory { get { return spellTargetCategory; } set { spellTargetCategory = value; } }
+
+        //defaults to modifying health
+        public ActionSpell(int range, int area, int value)
+        {
+            spellRange = range;
+            spellArea = area;
+            spellValue = value;
+            spellTargetCategory = "general";
+            spellTargetAttribute = "health";
+        }
+
+        public ActionSpell(int range, int area, int value, string att, string cat)
+        {
+            if ((range < 0) || (area < 0))
+                Console.WriteLine("Invalid spell range or area, could not initiate the instance;");
+            else {
+                spellRange = range;
+                spellArea = area;
+            }
+            spellValue = value;
+            spellTargetCategory = cat;
+            spellTargetAttribute = att;
+        }
+
+    }
+}
---
+++ Assets/_solace/GameAction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace solace
+{
+    
+    class GameAction
+    {
+        //class variables
+        private static int currentID { get { return currentID; } set { currentID = value; } }
+        private int actionID { get { return actionID; } set { actionID = value; } }
+        private string actionName { get { return actionName; } set { actionName = value; } }
+
+        //default constructor
+        public GameAction() {
+            actionName = "Unamed Action";
+            currentID += 1;
+            actionID = currentID;
+        }
+
+        //Name is the only modable field, ID's are assigned automatically
+        public GameAction(string newName)
+        {
+            actionName = newName;
+            currentID += 1;
+            actionID = currentID;
+        }
+
+        //initialize static variable, called before any other constructor
+        static GameAction()
+        {
+            currentID = 0;
+        }
+    }
+}
